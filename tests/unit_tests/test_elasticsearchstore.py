5316f8a2d02e5f9f231c0922a194ae68a7f10414
Axel Vivien <a.vivien@criteo.com>
2020-07-30 13:55:54 +0200

    fix and test get_experiment

--- tests/unit_tests/test_elasticsearchstore.py
+++ tests/unit_tests/test_elasticsearchstore.py
@@ -21,9 +21,18 @@ def test_create_experiment(document_mock):
 
 
 @mock.patch('elasticsearch_dsl.Document')
+def test_get_experiment(document_mock):
+    store = ElasticsearchStore("user", "password", "host", "port")
+    actual_experiment = store.get_experiment("1")
+    document_mock.assert_called_once_with(id=1)
+    experiment_mock = document_mock.return_value.get
+    assert experiment_mock.to_mlflow_entity() == actual_experiment
+
+
+@mock.patch('elasticsearch_dsl.Document')
 def test__get_run(document_mock):
     store = ElasticsearchStore("user", "password", "host", "port")
-    store._get_run(1)
+    actual_run = store._get_run("1")
     document_mock.assert_called_once_with(id=1)
     run_mock = document_mock.return_value.get
-    assert run_mock.meta.id == 1
+    assert run_mock == actual_run
