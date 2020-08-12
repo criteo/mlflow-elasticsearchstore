b21e491beceacfc5060ced0766421ea1e95263ab
Axel Vivien <a.vivien@criteo.com>
2020-08-12 18:51:12 +0200

    add retry

--- tests/scripts/restore_database.sh
+++ tests/scripts/restore_database.sh
@@ -1,11 +1,23 @@
-curl --location -u "elastic:password" --request PUT 'http://127.0.0.1:9200/_snapshot/my_fs_backup' \
+attempt_counter=0
+max_attempts=10
+
+until(curl --location -u "elastic:password" --request PUT 'http://127.0.0.1:9200/_snapshot/my_fs_backup' \
 --header 'Content-Type: application/json' \
 --data-raw '{
   "type": "fs",
   "settings": {
     "location": "/mount/backups/my_fs_backup_location"
   }
-}'
+}'); do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+        echo "Max attempts reached"
+        exit 1
+        fi
 
-curl --location -u "elastic:password"  --request POST 'http://127.0.0.1:9200/_snapshot/my_fs_backup/snapshot_1/_restore' \
+        printf '.'
+        attempt_counter=$(($attempt_counter+1))
+        sleep 5
+done
+
+curl  --location -u "elastic:password"  --request POST 'http://127.0.0.1:9200/_snapshot/my_fs_backup/snapshot_1/_restore' \
 --data-raw ''
