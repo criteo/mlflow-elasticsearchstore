fa2201970c8e9b5a2d2ad3dc0d156f7e6bd1a065
Axel Vivien <a.vivien@criteo.com>
2020-08-13 12:18:43 +0200

    working integartion test

--- tests/integration_tests/test_integration_elasticsearchstore.py
+++ tests/integration_tests/test_integration_elasticsearchstore.py
@@ -7,6 +7,6 @@ from mlflow_elasticsearchstore.elasticsearch_store import ElasticsearchStore
 def test_get_experiment():
     store = ElasticsearchStore(store_uri="elasticsearch://elastic:password@localhost:9200",
                                artifact_uri="viewfs://preprod-pa4/user/mlflow/mlflow_artifacts")
-    experiment_id = "FkpsfafgTAez5z_KUQyPvw"
+    experiment_id = "WrAh43MB26mjXDfbtLks"
     experiment = store.get_experiment(experiment_id=experiment_id)
-    assert experiment.meta.id == experiment_id
+    assert experiment.experiment_id == experiment_id
--- tests/scripts/initialize_database.sh
+++ tests/scripts/initialize_database.sh
@@ -5,7 +5,8 @@ docker images
 
 docker build --tag elastic_store .
 docker run \
-    --network=host \
+    --network=bridge \
+    -p 9200:9200 \
     -e ELASTIC_PASSWORD=password \
     -e xpack.security.enabled=true \
     -d \
